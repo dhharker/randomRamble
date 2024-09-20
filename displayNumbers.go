6715ade480a512aeff9289084eeadc1fc476f23e
David Harker <git@dhharker.co.uk>
2024-09-21 00:20:24 +0100

    add fyne

--- displayNumbers.go
+++ displayNumbers.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"log"
-	// "fyne.io/fyne/v2/app"
-	// "fyne.io/fyne/v2/widget"
 )
 
 func doDisplay(numbersChan chan float64, stopDisplayChan chan bool) {
 	var min, max float64
-
-	// a := app.New()
-	// w := a.NewWindow("Hello World")
-
-	// w.SetContent(widget.NewLabel("Hello World!"))
-	// w.ShowAndRun()
 
 	for {
 		select {
